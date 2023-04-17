fa57529cf839fb9fec7c89b38e19418f919d0f7b
salem.affa <salem.affa@mehriso.com>
2023-04-17 13:14:39 +0100

    chore: init sdk

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,26 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from '@kkiapay-org/react-native-sdk';
+// import { StyleSheet } from 'react-native';
+import { KkiapayProvider } from 'src';
+import TestComponent from './TestComponent';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <KkiapayProvider>
+      <TestComponent />
+    </KkiapayProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 60,
+//     height: 60,
+//     marginVertical: 20,
+//   },
+// });
